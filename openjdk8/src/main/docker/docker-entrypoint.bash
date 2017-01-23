c158f8d3ac87fb6f353ebcebfbc37d92f7e21651
Mike Eltsufin <meltsufin@google.com>
2017-01-23 17:39:03 -0500

    Allow sending SIGQUIT to JVM before shutdown to ouput thread dump

--- openjdk8/src/main/docker/docker-entrypoint.bash
+++ openjdk8/src/main/docker/docker-entrypoint.bash
@@ -2,7 +2,7 @@
 
 # source the supported feature JVM arguments
 source /setup-env.bash
-  
+
 # If the first argument is the java command
 if [ "java" = "$1" -o "$(which java)" = "$1" ] ; then
   # ignore it as java is the default command
@@ -12,11 +12,22 @@ fi
 # If the first argument is not executable
 if ! type "$1" &>/dev/null; then
   # then treat all arguments as arguments to the java command
-  
+
   # set the command line to java with the feature arguments and passed arguments
   set -- java $JAVA_OPTS "$@"
 fi
 
-# exec the entry point arguments as a command
-exec "$@"
 
+if [ -z "$SHUTDOWN_THREAD_DUMP" ]
+then
+  # exec the entry point arguments as a command
+  exec "$@"
+else
+  # capture the TERM signal and send a SIGQUIT first to generate the thread dump
+  trap 'kill -3 $PID; kill $PID' TERM
+  $@ &
+  PID=$!
+  wait $PID
+  wait $PID
+  exit $?
+fi
