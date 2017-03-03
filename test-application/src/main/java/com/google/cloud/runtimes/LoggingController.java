a39726f70673e599f9023e2392151e523e8b2d25
Alex Sloan <alexsloan@google.com>
2017-03-03 16:54:04 -0500

    Configure MonitoredResource to be enhanced with GAE environment settings

--- test-application/src/main/java/com/google/cloud/runtimes/LoggingController.java
+++ test-application/src/main/java/com/google/cloud/runtimes/LoggingController.java
@@ -2,15 +2,15 @@ package com.google.cloud.runtimes;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
+import com.google.cloud.MonitoredResource;
 import com.google.cloud.logging.GaeFlexLoggingEnhancer;
 import com.google.cloud.logging.LoggingHandler;
-
+import com.google.cloud.logging.LoggingHandler.Enhancer;
+import java.util.Collections;
+import java.util.logging.Logger;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collections;
-import java.util.logging.Logger;
 
 @RestController
 public class LoggingController {
@@ -25,13 +25,20 @@ public class LoggingController {
   @RequestMapping(path = "/logging", method = POST)
   public void logging(@RequestBody LoggingRequest request) {
 
+    // Explicitly create and enhance the monitored resource. This is necessary because the
+    // LoggingHandler does not do this automatically for non-default resourceTypes
+    Enhancer gaeFlexEnhancer = new GaeFlexLoggingEnhancer();
+    MonitoredResource.Builder resourceBuilder = MonitoredResource.newBuilder("gae_app");
+    gaeFlexEnhancer.enhanceMonitoredResource(resourceBuilder);
+
     // create log handler for the given log_name
-    LoggingHandler loggingHandler = new LoggingHandler(request.log_name, null, null,
-        Collections.singletonList(new GaeFlexLoggingEnhancer()));
+    LoggingHandler loggingHandler = new LoggingHandler(request.log_name, null,
+        resourceBuilder.build(),
+        Collections.singletonList(gaeFlexEnhancer));
     LoggingHandler.addHandler(logger, loggingHandler);
 
-    // send the token to stackdriver logging
-    logger.severe("payload.token: " + request.token);
+    // write the log message
+    logger.info("payload.token from request: " + request.token);
 
     // remove the handler
     logger.removeHandler(loggingHandler);
