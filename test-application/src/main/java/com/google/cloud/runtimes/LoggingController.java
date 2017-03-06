27dab94c700feb33288c99b973482ab6deac1c79
Alex Sloan <alexsloan@google.com>
2017-03-06 15:30:50 -0500

    Configure logging via logging.properties

--- test-application/src/main/java/com/google/cloud/runtimes/LoggingController.java
+++ test-application/src/main/java/com/google/cloud/runtimes/LoggingController.java
@@ -2,11 +2,6 @@ package com.google.cloud.runtimes;
 
 import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
-import com.google.cloud.MonitoredResource;
-import com.google.cloud.logging.GaeFlexLoggingEnhancer;
-import com.google.cloud.logging.LoggingHandler;
-import com.google.cloud.logging.LoggingHandler.Enhancer;
-import java.util.Collections;
 import java.util.logging.Logger;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,24 +19,8 @@ public class LoggingController {
 
   @RequestMapping(path = "/logging", method = POST)
   public void logging(@RequestBody LoggingRequest request) {
-
-    // Explicitly create and enhance the monitored resource. This is necessary because the
-    // LoggingHandler does not do this automatically for non-default resourceTypes
-    Enhancer gaeFlexEnhancer = new GaeFlexLoggingEnhancer();
-    MonitoredResource.Builder resourceBuilder = MonitoredResource.newBuilder("gae_app");
-    gaeFlexEnhancer.enhanceMonitoredResource(resourceBuilder);
-
-    // create log handler for the given log_name
-    LoggingHandler loggingHandler = new LoggingHandler(request.log_name, null,
-        resourceBuilder.build(),
-        Collections.singletonList(gaeFlexEnhancer));
-    LoggingHandler.addHandler(logger, loggingHandler);
-
     // write the log message
     logger.info("payload.token from request: " + request.token);
-
-    // remove the handler
-    logger.removeHandler(loggingHandler);
   }
 
 }
