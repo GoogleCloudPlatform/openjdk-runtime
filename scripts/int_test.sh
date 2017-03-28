88d521bf9d735491d66789b3d4a938ea52ae8a7a
Alex Sloan <alexsloan@google.com>
2017-03-28 13:06:45 -0400

    Fix the hello world test

--- scripts/int_test.sh
+++ scripts/int_test.sh
@@ -7,19 +7,19 @@ projectRoot=$dir/..
 testAppDir=$projectRoot/test-application
 deployDir=$testAppDir/target/deploy
 
-## build test app
-#pushd $testAppDir
-#mvn clean install
-#popd
-#
-## deploy to app engine
-#pushd $deployDir
-#export STAGING_IMAGE=gcr.io/google-appengine/openjdk # TODO this should be an arg to this script
-#envsubst < Dockerfile.in > Dockerfile
-#echo "Deploying with dockerfile:"
-#cat Dockerfile
-#gcloud app deploy -q
-#popd
+# build test app
+pushd $testAppDir
+mvn clean install
+popd
+
+# deploy to app engine
+pushd $deployDir
+export STAGING_IMAGE=gcr.io/google-appengine/openjdk # TODO this should be an arg to this script
+envsubst < Dockerfile.in > Dockerfile
+echo "Deploying with dockerfile:"
+cat Dockerfile
+gcloud app deploy -q
+popd
 
 DEPLOYED_APP_URL="http://$(gcloud app describe | grep defaultHostname | awk '{print $2}')"
 echo "Running integration tests on application that is deployed at $DEPLOYED_APP_URL"
