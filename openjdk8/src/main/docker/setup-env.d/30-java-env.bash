dfe79b94f685905bdbf7b24f6638978eefe59c5d
Damien Cassan <damiencassan@google.com>
2017-05-09 12:24:28 -0400

    Search memory limit for the JVM in the environment variables

--- openjdk8/src/main/docker/setup-env.d/30-java-env.bash
+++ openjdk8/src/main/docker/setup-env.d/30-java-env.bash
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+GetAvailableMemory () {
+  if [ -n "$MEMORY_LIMIT" ]; then
+    # As Kubernetes give the memory in byte we have to convert it to MB
+    echo $((MEMORY_LIMIT / 1000000))
+  else
+    echo "$(awk '/MemTotal/{ print int($2/1024-400) }' /proc/meminfo)"
+  fi
+}
+
 # Setup default Java Options
 export JAVA_TMP_OPTS=${JAVA_TMP_OPTS:-$( if [[ -z ${TMPDIR} ]]; then echo ""; else echo "-Djava.io.tmpdir=$TMPDIR"; fi)}
-export GAE_MEMORY_MB=${GAE_MEMORY_MB:-$(awk '/MemTotal/{ print int($2/1024-400) }' /proc/meminfo)}
+export GAE_MEMORY_MB=${GAE_MEMORY_MB:-$(GetAvailableMemory)}
 export HEAP_SIZE_RATIO=${HEAP_SIZE_RATIO:-"80"}
 export HEAP_SIZE_MB=${HEAP_SIZE_MB:-$(expr ${GAE_MEMORY_MB} \* ${HEAP_SIZE_RATIO} / 100)}
 export JAVA_HEAP_OPTS=${JAVA_HEAP_OPTS:-"-Xms${HEAP_SIZE_MB}M -Xmx${HEAP_SIZE_MB}M"}
