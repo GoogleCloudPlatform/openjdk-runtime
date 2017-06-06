e4953af93640383ef8a186fb5f3c33c8b0f81f31
Mike Eltsufin <meltsufin@google.com>
2017-06-06 11:47:53 -0400

    Fix DBG_ENABLE checking and add tests

--- openjdk8/src/main/docker/setup-env.d/20-debug-env.bash
+++ openjdk8/src/main/docker/setup-env.d/20-debug-env.bash
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 # configure Cloud Debugger
+
+export DBG_SCRIPT_PATH=${DBG_SCRIPT_PATH:-"/opt/cdbg/format-env-appengine-vm.sh"}
+
 if [ "$PLATFORM" = "gae" ]; then
   export DBG_AGENT=
-  export DBG_ENABLE=${DBG_ENABLE:-$( if [[ -z "${CDBG_DISABLE}" && -x /opt/cdbg/format-env-appengine-vm.sh ]] ; then echo true; else echo false ; fi )}
+  export DBG_ENABLE=${DBG_ENABLE:-$( if [[ -z "${CDBG_DISABLE}" && -x "${DBG_SCRIPT_PATH}" ]] ; then echo true; else echo false ; fi )}
 fi
 
-if [ "$DBG_ENABLE" = "true" ]; then
+if [ "$DBG_ENABLE" = "true" -o "$DBG_ENABLE" = "True" ]; then
   unset CDBG_DISABLE
-  DBG_AGENT="$(/opt/cdbg/format-env-appengine-vm.sh)"
+  DBG_AGENT="$($DBG_SCRIPT_PATH)"
 fi
