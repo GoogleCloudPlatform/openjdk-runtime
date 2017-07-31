993170258a47c3647bb9aeb8b3f0e1f93112682e
balopat <balintp@google.com>
2017-07-31 10:53:31 -0400

    fixed faulty structure test

--- openjdk8/src/test/workspace/test-no-java-7.bash
+++ openjdk8/src/test/workspace/test-no-java-7.bash
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-POSSIBLE_JAVA_7_PKGS=`apt-cache dump | grep -E '(java|jre|jdk)' | grep Package | grep 7 | awk '{ print $2 }'`
-for PKG in $POSSIBLE_JAVA_7_PKGS;
+JAVA_MAJOR_VERSION=7
+
+POSSIBLE_JAVA_PKGS=`apt-cache dump | grep -E '(java|jre|jdk)' | grep Package | grep $JAVA_MAJOR_VERSION | awk '{ print $2 }'`
+for PKG in $POSSIBLE_JAVA_PKGS;
   do
-   apt list | grep $PKG || echo "OK. $PKG is not installed"
+   apt list | grep $PKG && echo "NOT OK. $PKG is installed" || echo "OK. $PKG is not installed"
   done
