b0b64f3d95f982538bfbd188e0cd1898282f9289
balopat <balintp@google.com>
2017-08-01 13:40:04 -0400

    simplified blacklisted packages definition

--- openjdk8/src/test/workspace/test-blacklisted-pkgs.bash
+++ openjdk8/src/test/workspace/test-blacklisted-pkgs.bash
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-BLACKLIST="jvm-7-avian-jre"
-BLACKLIST="java7-runtime-headless $BLACKLIST"
-BLACKLIST="python2.7-pyjavaproperties $BLACKLIST"
-BLACKLIST="libjsr107cache-java $BLACKLIST"
-BLACKLIST="libbiojava1.7-java $BLACKLIST"
-BLACKLIST="oracle-java7-installer $BLACKLIST"
-BLACKLIST="oracle-java7-jdk $BLACKLIST"
-BLACKLIST="openjdk-7-jre-zero $BLACKLIST"
-BLACKLIST="sun-java7-jre $BLACKLIST"
-BLACKLIST="java7-sdk $BLACKLIST"
-BLACKLIST="libdb4.7-java-dev $BLACKLIST"
-BLACKLIST="openjdk-7-jdk $BLACKLIST"
-BLACKLIST="java7-sdk-headless $BLACKLIST"
-BLACKLIST="openjdk-7-jre-headless $BLACKLIST"
-BLACKLIST="java7-runtime $BLACKLIST"
-BLACKLIST="libtomcat7-java $BLACKLIST"
-BLACKLIST="openjdk-7-dbg $BLACKLIST"
-BLACKLIST="openjdk-7-jre-dcevm $BLACKLIST"
-BLACKLIST="openjdk-7-jre-lib $BLACKLIST"
-BLACKLIST="uwsgi-plugin-jvm-openjdk-7 $BLACKLIST"
-BLACKLIST="openjdk-7-jre $BLACKLIST"
-BLACKLIST="openjdk-7-source $BLACKLIST"
-BLACKLIST="icedtea-7-jre-jamvm $BLACKLIST"
-BLACKLIST="openjdk-7-demo $BLACKLIST"
-BLACKLIST="openjdk-7-doc $BLACKLIST"
-BLACKLIST="libnb-platform7-devel-java $BLACKLIST"
-BLACKLIST="uwsgi-plugin-jwsgi-openjdk-7 $BLACKLIST"
+BLACKLIST="\
+jvm-7-avian-jre \
+java7-runtime-headless \
+python2.7-pyjavaproperties \
+libjsr107cache-java \
+libbiojava1.7-java \
+oracle-java7-installer \
+oracle-java7-jdk \
+openjdk-7-jre-zero \
+sun-java7-jre \
+java7-sdk \
+libdb4.7-java-dev \
+openjdk-7-jdk \
+java7-sdk-headless \
+openjdk-7-jre-headless \
+java7-runtime \
+libtomcat7-java \
+openjdk-7-dbg \
+openjdk-7-jre-dcevm \
+openjdk-7-jre-lib \
+uwsgi-plugin-jvm-openjdk-7 \
+openjdk-7-jre \
+openjdk-7-source \
+icedtea-7-jre-jamvm \
+openjdk-7-demo \
+openjdk-7-doc \
+libnb-platform7-devel-java \
+uwsgi-plugin-jwsgi-openjdk-7"
+
 
 for PKG in $BLACKLIST;
   do
