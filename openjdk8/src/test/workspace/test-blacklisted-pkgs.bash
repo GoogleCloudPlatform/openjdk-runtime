5ddd0a314c88e360081c4ee2775f9a38a24fbbce
Balint Pato <balopat@users.noreply.github.com>
2017-08-01 14:10:01 -0400

    adds structure test to ensure no java 7 runtime gets pulled in (#123)
    * [#55] added structure test to ensure no java 7 runtime gets pulled into the image
    
    * fixed faulty structure test
    
    * rewrote to explicit blacklist checking instead of inferring jre7 packages
    
    * simplified blacklisted packages definition

---
+++ openjdk8/src/test/workspace/test-blacklisted-pkgs.bash
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+BLACKLIST="\
+jvm-7-avian-jre \
+java7-runtime-headless \
+python2.7-pyjavaproperties \
+libjsr107cache-java \
+libbiojava1.7-java \
+oracle-java7-installer \
+oracle-java7-jdk \
+openjdk-7-jre-zero \
+sun-java7-jre \
+java7-sdk \
+libdb4.7-java-dev \
+openjdk-7-jdk \
+java7-sdk-headless \
+openjdk-7-jre-headless \
+java7-runtime \
+libtomcat7-java \
+openjdk-7-dbg \
+openjdk-7-jre-dcevm \
+openjdk-7-jre-lib \
+uwsgi-plugin-jvm-openjdk-7 \
+openjdk-7-jre \
+openjdk-7-source \
+icedtea-7-jre-jamvm \
+openjdk-7-demo \
+openjdk-7-doc \
+libnb-platform7-devel-java \
+uwsgi-plugin-jwsgi-openjdk-7"
+
+
+for PKG in $BLACKLIST;
+  do
+   dpkg -l | grep '^.i' | grep $PKG > /dev/null && echo "NOT OK. $PKG is installed" || echo "OK. $PKG is not installed"
+  done
