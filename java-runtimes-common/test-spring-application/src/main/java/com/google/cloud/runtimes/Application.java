be9d5d36425187fadde2812c61e73ba7d56868ce
Alex Sloan <alexsloan@google.com>
2017-09-13 11:24:16 -0400

    Fix war deployments for test app

--- java-runtimes-common/test-spring-application/src/main/java/com/google/cloud/runtimes/Application.java
+++ java-runtimes-common/test-spring-application/src/main/java/com/google/cloud/runtimes/Application.java
@@ -2,10 +2,18 @@ package com.google.cloud.runtimes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(Application.class);
+	}
+
 }
