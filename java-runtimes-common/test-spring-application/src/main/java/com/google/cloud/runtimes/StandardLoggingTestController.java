5b7afb9aae9c4cb9eb89b5cbd71af8668f98827c
balopat <balintp@google.com>
2017-09-14 18:07:56 -0400

    temp - made it working with exclusion

--- java-runtimes-common/test-spring-application/src/main/java/com/google/cloud/runtimes/StandardLoggingTestController.java
+++ java-runtimes-common/test-spring-application/src/main/java/com/google/cloud/runtimes/StandardLoggingTestController.java
@@ -1,19 +1,10 @@
 package com.google.cloud.runtimes;
 
-import com.google.cloud.MonitoredResource;
-import com.google.cloud.logging.LogEntry;
-import com.google.cloud.logging.Logging;
-import com.google.cloud.logging.Payload;
-import com.google.cloud.logging.Severity;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -21,10 +12,6 @@ import static org.springframework.web.bind.annotation.RequestMethod.POST;
 
 @RestController
 public class StandardLoggingTestController {
-
-    @Autowired
-    @Lazy
-    private Logging logging;
 
     private static Logger LOG = Logger.getLogger(StandardLoggingTestController.class.getName());
 
@@ -36,11 +23,9 @@ public class StandardLoggingTestController {
             this.level = level;
         }
 
-
         public void setToken(String token) {
             this.token = token;
         }
-
 
         @Override
         public String toString() {
@@ -53,10 +38,9 @@ public class StandardLoggingTestController {
 
     @RequestMapping(path = "/logging_standard", method = POST)
     public String handleStandardLoggingTestRequest(@RequestBody StandardLoggingTestRequest standardLoggingTestRequest) throws IOException, InterruptedException {
-        LOG.info(String.valueOf(standardLoggingTestRequest));
+        LOG.info("Received: " + String.valueOf(standardLoggingTestRequest));
         LOG.log(Level.parse(standardLoggingTestRequest.level), standardLoggingTestRequest.token);
-        LOG.info("Log written using standard JUL.");
-        return "OK";
+        return "appengine.googleapis.com%2Fstdout";
     }
 
 
