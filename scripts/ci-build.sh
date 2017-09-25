9ed77d4b2b5661c056de7196ca82e699f56b46d1
Alex Sloan <alexsloan@google.com>
2017-09-25 11:41:58 -0400

    Update ci-build with new arguments

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -21,18 +21,16 @@ set -e
 dir=$(dirname "$0")
 
 # downloads, unpacks, installs the cloud SDK
-source "$dir"/gcloud-init.sh
+#source "$dir"/gcloud-init.sh
 
-cd github/openjdk-runtime
+#cd github/openjdk-runtime
 TAG_SUFFIX=$(git rev-parse --short HEAD)
 
-echo "Invoking build.sh with DOCKER_NAMESPACE=$DOCKER_NAMESPACE, MODULE=$MODULE, TAG_SUFFIX=$TAG_SUFFIX"
-source ./scripts/build.sh --docker-namespace "$DOCKER_NAMESPACE" --module "$MODULE" --tag-suffix "$TAG_SUFFIX"
+echo "Invoking build.sh with GCP_PROJECT=$GCP_PROJECT, MODULE=$MODULE, TAG_SUFFIX=$TAG_SUFFIX"
+source ./scripts/build.sh --publishing-project "$GCP_PROJECT" --module "$MODULE" --tag-suffix "$TAG_SUFFIX"
 
 if [ -z "$IMAGE" ]; then
   echo "Error: \$IMAGE not defined. It should be exported by build.sh script."
   exit 1
 fi
 
-echo "Running integration tests on image: $IMAGE"
-./scripts/integration_test.sh "$IMAGE"
