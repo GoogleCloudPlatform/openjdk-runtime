645a27d9bf0ef97d3d302af64b659a72354b2a12
Don McCasland <don@nestlabs.com>
2019-04-04 10:30:00 -0700

    Add tools (#187)
    * Adding build scripts
    
    * Fixing deploy check script
    
    * Removing openjdk9 that fails Travis CI
    
    * Resolving conflicts

---
+++ scripts/deploy-check-openjdk8.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+export KOKORO_GITHUB_DIR=${KOKORO_ROOT}/src/github
+source ${KOKORO_GFILE_DIR}/kokoro/common.sh
+
+cd ${KOKORO_GITHUB_DIR}/${SAMPLE_APP_DIRECTORY}
+
+mvn install --batch-mode -DskipTests -Pruntime.java,deploy.war
+
+cat <<EOF > ${KOKORO_GITHUB_DIR}/${SAMPLE_APP_DIRECTORY}/app.yaml
+runtime: java
+env: flex
+runtime_config:
+  server: jetty9
+resources:
+  memory_gb: 2.5
+EOF
+
+cd ${KOKORO_GFILE_DIR}/appengine/integration_tests
+
+sudo /usr/local/bin/pip install --upgrade -r requirements.txt
+
+if [ -f ${KOKORO_GITHUB_DIR}/${SAMPLE_APP_DIRECTORY}/requirements.txt ]
+then
+  sudo /usr/local/bin/pip install --upgrade -r ${KOKORO_GITHUB_DIR}/${SAMPLE_APP_DIRECTORY}/requirements.txt
+fi
+
+export DEPLOY_LATENCY_PROJECT='cloud-deploy-latency'
+
+skip_flag=""
+
+if [ "${SKIP_CUSTOM_LOGGING_TESTS}" = "true" -o "${SKIP_BUILDERS}" = "true" ]; then
+  skip_flag="$skip_flag --skip-builders"
+fi
+
+if [ "${SKIP_XRT}" = "true" ]; then
+  skip_flag="$skip_flag --skip-xrt"
+fi
+
+python deploy_check.py -d ${KOKORO_GITHUB_DIR}/${SAMPLE_APP_DIRECTORY} -l ${LANGUAGE} ${skip_flag}
