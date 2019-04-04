63ee67d18b1baf1d5c0350f85bd1fabdc6fe1145
Don McCasland <donmccasland@google.com>
2019-04-04 15:02:52 -0700

    Fixing release script path

--- scripts/release-openjdk.sh
+++ scripts/release-openjdk.sh
@@ -2,7 +2,7 @@
 export KOKORO_GITHUB_DIR=${KOKORO_ROOT}/src/github
 source ${KOKORO_GFILE_DIR}/kokoro/common.sh
 
-cd ${KOKORO_GITHUB_DIR}
+cd ${KOKORO_GITHUB_DIR}/openjdk-runtime
 
 params="--publishing-project ${PUBLISHING_PROJECT} --staging-project ${GCP_TEST_PROJECT} --module ${MODULE}"
 if [ -n "${TAG_SUFFIX}" ]; then
